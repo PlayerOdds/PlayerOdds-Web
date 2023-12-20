cfc104ee84e7d0ae6778527e9a96023eea062b36
BVDrewPick <drew@blueverse.club>
2023-12-19 19:41:12 -0600

    Turned latest into components

---
+++ src/components/TableRow.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import fanduelLogo from '../images/Fanduel_Shield_2022.png'; // Adjust the path as necessary
+
+const TableRow = ({ item }) => {
+  const tdStyle = { padding: '8px', border: '1px solid #ddd', textAlign: 'center' };
+
+  return (
+    <tr style={{ backgroundColor: item.index % 2 === 0 ? '#f2f2f2' : 'transparent' }}>
+      <td style={tdStyle}>{item.game_id}</td>
+      <td style={tdStyle}>
+        {item.bookie_key === 'fanduel' ? (
+          <img src={fanduelLogo} alt="FanDuel Logo" style={{ height: '30px', width: 'auto' }} />
+        ) : (
+          item.bookie_key
+        )}
+      </td>
+      <td style={tdStyle}>{item.market_key}</td>
+      <td style={tdStyle}>{item.time_created}</td>
+      <td style={tdStyle}>{item.handicap}</td>
+      <td style={tdStyle}>{item.odds}</td>
+      <td style={tdStyle}>{item.prop_name}</td>
+      <td style={tdStyle}>{item.prop_description}</td>
+      <td style={tdStyle}>{item.participant_name}</td>
+    </tr>
+  );
+};
+
+export default TableRow;
--- src/components/datadisplay.js
+++ src/components/datadisplay.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import fanduelLogo from '../images/Fanduel_Shield_2022.png'; // Adjust the path as necessary
+import fanduelLogo from '../images/Fanduel_Shield_2022.png';
+import TableHeader from './TableHeader';
+import TableRow from './TableRow';
 
-// URL for the FanDuel logo - replace with the actual URL
 
 
 const styles = {
@@ -11,25 +12,7 @@ const styles = {
     margin: '20px auto',
     borderCollapse: 'collapse',
   },
-  th: {
-    backgroundColor: '#f2f2f2',
-    color: 'black',
-    padding: '10px',
-    border: '1px solid #ddd',
-  },
-  td: {
-    padding: '8px',
-    border: '1px solid #ddd',
-    textAlign: 'center', // Center align content in cells
-  },
-  logo: {
-    height: '30px', // Adjust the size as needed
-    width: 'auto'
-  },
-  tr: {
-    ':nth-child(even)': { backgroundColor: '#f2f2f2' }
-  }
-};
+}
 
 const DataDisplay = ({ data }) => {
   if (!data || !Array.isArray(data)) {
@@ -39,37 +22,11 @@ const DataDisplay = ({ data }) => {
   return (
     <table style={styles.table}>
       <thead>
-        <tr>
-          <th style={styles.th}>Game ID</th>
-          <th style={styles.th}>Bookie Key</th>
-          <th style={styles.th}>Market Key</th>
-          <th style={styles.th}>Time Created</th>
-          <th style={styles.th}>Handicap</th>
-          <th style={styles.th}>Odds</th>
-          <th style={styles.th}>Prop Name</th>
-          <th style={styles.th}>Prop Description</th>
-          <th style={styles.th}>Participant Name</th>
-        </tr>
+        <TableHeader />
       </thead>
       <tbody>
         {data.map((item, index) => (
-          <tr key={index} style={styles.tr}>
-            <td style={styles.td}>{item.game_id}</td>
-            <td style={styles.td}>
-              {item.bookie_key === 'fanduel' ? (
-                <img src={fanduelLogo} alt="FanDuel Logo" style={styles.logo} />
-              ) : (
-                item.bookie_key
-              )}
-            </td>
-            <td style={styles.td}>{item.market_key}</td>
-            <td style={styles.td}>{item.time_created}</td>
-            <td style={styles.td}>{item.handicap}</td>
-            <td style={styles.td}>{item.odds}</td>
-            <td style={styles.td}>{item.prop_name}</td>
-            <td style={styles.td}>{item.prop_description}</td>
-            <td style={styles.td}>{item.participant_name}</td>
-          </tr>
+          <TableRow key={index} item={item} />
         ))}
       </tbody>
     </table>
