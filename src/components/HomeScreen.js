8c0f8cff3afc58c849f67a3a834b7f4ea962ac0e
Cavan Natal <82979308+cavannatal@users.noreply.github.com>
2024-01-25 17:49:46 -0600

    Updated

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -12,11 +12,11 @@ const HomeScreen = () => {
                     <div className='md:w-3/5 flex flex-col justify-center'>
                         {/* Modified Heading */}
                         <h2 className='text-center text-6xl sm:text-7xl md:text-6xl font-bold text-white mb-6 leading-tight md:leading-normal'>
-                            Giving you the <span className='text-[#17D475]'>Beneficial Edge.</span>
+                            Giving you the <span className='text-[#17D475]'>Beneficial Edge</span>
                         </h2>
                         {/* Paragraph */}
                         <p className='text-center text-zinc-300 text-lg md:text-2xl mb-8 leading-tight md:leading-normal'>
-                            Odds calculated and Provided straight to you.
+                            Odds Calculated and Provided Straight to you.
                         </p>
                     </div>
 
