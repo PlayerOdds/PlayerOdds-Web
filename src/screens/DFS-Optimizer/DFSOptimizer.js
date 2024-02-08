429788e49a132f42dc2457288c09a606f533ece8
BVDrewPick <drew@blueverse.club>
2024-02-08 14:04:25 -0600

    Samll Changes

--- src/screens/DFS-Optimizer/DFSOptimizer.js
+++ src/screens/DFS-Optimizer/DFSOptimizer.js
@@ -3,17 +3,15 @@ import { useState } from 'react';
 import { TableContainer, Table, Paper, Autocomplete, TextField, TableBody } from '@mui/material';
 import TableRowComponent from './components/TableRowComponent';
 import TableHeaderComponent from './components/TableHeaderComponent';
-import { rows, createData } from './components/data'; // Assume data is extracted to a separate file
+import { rows, createData } from './components/data'; 
 
 export default function BasicTable() {
-  // State and selection logic remains the same
   const [selectedPlayer, setSelectedPlayer] = useState(null);
   const playerNames = rows.map(row => row.name);
   const filteredRows = selectedPlayer ? rows.filter(row => row.name === selectedPlayer) : rows;
 
   return (
     <>
-      {/* Selection logic remains the same */}
       <TableContainer component={Paper} sx={{ maxWidth: '100%', margin: 'auto', boxShadow: 3, background: '#03161C' }}>
         <Table sx={{ minWidth: 100 }} aria-label="simple table">
           <TableHeaderComponent />
