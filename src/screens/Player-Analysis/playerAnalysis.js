02a17264e493a5ad3516f057e3448d762320e231
BVDrewPick <drew@blueverse.club>
2024-02-10 17:04:24 -0600

    Changed Text Colot

--- src/screens/Player-Analysis/playerAnalysis.js
+++ src/screens/Player-Analysis/playerAnalysis.js
@@ -122,31 +122,31 @@ const renderStatsTable = (player) => (
   <table className="min-w-full table-fixed text-center">
     <thead>
       <tr>
-        <th className="px-4 py-2">Category</th>
-        <th className="px-4 py-2">Games Played</th>
-        <th className="px-4 py-2">Rush Att</th>
-        <th className="px-4 py-2">Rush Yds</th>
-        <th className="px-4 py-2">Rush TDs</th>
-        <th className="px-4 py-2">Rec TGT</th>
-        <th className="px-4 py-2">Rec</th>
-        <th className="px-4 py-2">Rec Yds</th>
-        <th className="px-4 py-2">Rec TD</th>
+        <th className="px-4 py-2 text-white"></th>
+        <th className="px-4 py-2 text-white">Games Played</th>
+        <th className="px-4 py-2 text-white">Rush Att</th>
+        <th className="px-4 py-2 text-white">Rush Yds</th>
+        <th className="px-4 py-2 text-white">Rush TDs</th>
+        <th className="px-4 py-2 text-white">Rec TGT</th>
+        <th className="px-4 py-2 text-white">Rec</th>
+        <th className="px-4 py-2 text-white">Rec Yds</th>
+        <th className="px-4 py-2 text-white">Rec TD</th>
       </tr>
     </thead>
     <tbody>
       {['season', 'last_10', 'last_5', 'previous', 'vs_opponent'].map((category) => (
         <tr key={category}>
-          <td className="border px-4 py-2">{category.toUpperCase()}</td>
+          <td className="border px-4 py-2 text-white">{category.toUpperCase()}</td>
           {player[category].map((stat) => (
             <>
-              <td className="border px-4 py-2">{stat.Games_Played}</td>
-              <td className="border px-4 py-2">{stat.Rush_Att}</td>
-              <td className="border px-4 py-2">{stat.Rush_Yards}</td>
-              <td className="border px-4 py-2">{stat.Rush_TD}</td>
-              <td className="border px-4 py-2">{stat.Rec_Tgt}</td>
-              <td className="border px-4 py-2">{stat.Receptions}</td>
-              <td className="border px-4 py-2">{stat.Rec_Yards}</td>
-              <td className="border px-4 py-2">{stat.Rec_TD}</td>
+              <td className="border px-4 py-2 text-white">{stat.Games_Played}</td>
+              <td className="border px-4 py-2 text-white">{stat.Rush_Att}</td>
+              <td className="border px-4 py-2 text-white">{stat.Rush_Yards}</td>
+              <td className="border px-4 py-2 text-white">{stat.Rush_TD}</td>
+              <td className="border px-4 py-2 text-white">{stat.Rec_Tgt}</td>
+              <td className="border px-4 py-2 text-white">{stat.Receptions}</td>
+              <td className="border px-4 py-2 text-white">{stat.Rec_Yards}</td>
+              <td className="border px-4 py-2 text-white">{stat.Rec_TD}</td>
             </>
           ))}
         </tr>
