35d9effab3d4fa33dd93508118417ff36d8e79d3
BVDrewPick <drew@blueverse.club>
2024-02-13 17:38:41 -0600

    Added Extra data for testing mapping

--- src/screens/DFS-Optimizer/components/newdata.js
+++ src/screens/DFS-Optimizer/components/newdata.js
@@ -16,5 +16,22 @@ export const playersData = [
         
       ],
     },
+    {
+      name: 'Jackson Mahomes',
+      teamInfo: 'NFL / KC / QB / #69',
+      matchup: 'KC @ ORL - 2/13 7:30 PM ET',
+      betType: 'POINTS',
+      line: 10.5,
+      pick: 'UNDER',
+      odds: '53.93%',
+      oddsValue: '-117.1',
+      platforms: [
+        { platform: 'Q', line: 10.5, odds: '-119' },
+        { platform: 'S', line: 13.5, odds: '+129' },
+        { platform: 'W', line: 12.5, odds: '-119' },
+        { platform: 'G', line: 12.5, odds: '-119' },
+        
+      ],
+    },
   ];
   
