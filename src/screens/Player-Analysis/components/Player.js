3caf72f103615ebd9dcc1c575aa1e517e9d6d8a6
BVDrewPick <drew@blueverse.club>
2024-02-20 15:16:39 -0600

    Added colors

--- src/screens/Player-Analysis/components/Player.js
+++ src/screens/Player-Analysis/components/Player.js
@@ -3,8 +3,8 @@ import PlayerDetails from "./PlayerDetails";
 
 const Player = ({ player, togglePlayer, openPlayerName }) => {
   return (
-    <div className="mt-2 border bg-emerald-950">
-      <div className='cursor-pointer p-4 text-white flex items-center border' onClick={() => togglePlayer(player.name)}>
+    <div className="mt-2 border bg-emerald-950" style={{background: ' #1f1f1e'}}>
+      <div className='cursor-pointer p-4 text-white flex items-center border' style={{background: 'linear-gradient(to right, red 0%, red 5%, #1f1f1e 30%, #1f1f1e 100%)'}} onClick={() => togglePlayer(player.name)}>
         {player.image && <img src={player.image} alt={player.name} style={{ maxWidth: '75px', maxHeight: '75px', marginRight: '10px' }} />} 
         <div>{player.name}</div>
       </div>
