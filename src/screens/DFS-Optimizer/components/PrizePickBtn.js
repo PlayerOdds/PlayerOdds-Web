ae0631a09ceb86163ab67bbcae71c5c80d3f6cfd
BVDrewPick <drew@blueverse.club>
2024-03-11 19:10:11 -0500

    Changes

--- src/screens/DFS-Optimizer/components/PrizePickBtn.js
+++ src/screens/DFS-Optimizer/components/PrizePickBtn.js
@@ -4,9 +4,12 @@ import { PrizPicksLogo } from '../../../images/ImageRepository';
 const PrizePickBtn = () => {
     return (
         <div>
-             <button className="flex items-center bg-blue-500 text-white font-bold py-2 px-4 rounded-xl hover:bg-blue-700 transition duration-150 ease-in-out">
+            <button 
+                style={{ backgroundColor: '#03FD91' }} 
+                className="flex items-center text-black font-bold py-2 px-4 rounded-xl hover:bg-blue-700 transition duration-150 ease-in-out"
+            >
                 <img src={PrizPicksLogo} alt="Icon" className="w-6 h-6 mr-2"/>
-                    Prize Picks
+                Prize Picks
             </button>
         </div>
     );        
