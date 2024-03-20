8f7c392b147cb5695cf31d75bb812a7b680d16a3
BVDrewPick <drew@blueverse.club>
2024-03-20 14:39:59 -0500

    Database Changes

--- src/screens/Player-Analysis/components/gamesData.js
+++ src/screens/Player-Analysis/components/gamesData.js
@@ -2,7 +2,7 @@ import {Mahomes, Purdy, Niners, Chiefs} from '../../../images/ImageRepository'
 
 export const games = [
   {
-    league: 'NFL',
+    league: 'NBA',
     todays_games:[
       { 
       id: 'game1',
@@ -1218,5 +1218,221 @@ export const games = [
       ],
     },
   ],
+  
   },
-];
+  {
+    league: 'NBA',
+    todays_games:[
+      { 
+      id: 'game1',
+      title: 'San Francisco 49ers at Kansas City Chiefs',
+      teamInfo: 'NFL / SF ',
+      date: '2/11 - 6:30 PM EST',
+      players: [
+        {
+          name: 'Brock Purdy - SF',
+          image: Purdy, 
+          teamLogo: Niners,
+          stat:[
+            {
+              book:'PrizePicks',
+              stat: "Passing Yards",
+              Stat_Value: "276.5",
+              Over_Under: "Over"
+            },
+          {
+              book:'PrizePicks',
+              stat: "Rushing Yards",
+              Stat_Value: "27.5",
+              Over_Under: "Under"
+            },
+            {
+              book:'PrizePicks',
+              stat: "Completions",
+              Stat_Value: "276.5",
+              Over_Under: "Over"
+            },
+          {
+              book:'PrizePicks',
+              stat: "Passing TD's",
+              Stat_Value: "1.5",
+              Over_Under: "Under"
+            },
+            {
+              book:'PrizePicks',
+              stat: "Total Touchdowns",
+              Stat_Value: "2.5",
+              Over_Under: "Over"
+            },
+          {
+              book:'PrizePicks',
+              stat: "Turnover",
+              Stat_Value: "0.5",
+              Over_Under: "Under"
+            }
+          ],
+          previousGames: ['300 yards, 3 TDs', '250 yards, 2 TDs'],
+          season:[{
+            Games_Played:['14'],
+            Rush_Att:['0.1'],
+            Rush_Yards:['-0.1'],
+            Rush_TD:['0'],
+            Rec_Tgt:['2.9'],
+            Receptions:['1.8'],
+            Rec_Yards:['23.9'],
+            Rec_TD:['0.1'],
+          }],
+          last_10:[{
+            Games_Played:['10'],
+            Rush_Att:['0.1'],
+            Rush_Yards:['-0.1'],
+            Rush_TD:['0'],
+            Rec_Tgt:['3.3'],
+            Receptions:['2'],
+            Rec_Yards:['23.3'],
+            Rec_TD:['0.1'],
+          }],
+          last_5:[{
+            Games_Played:['5'],
+            Rush_Att:['0.2'],
+            Rush_Yards:['-0.2'],
+            Rush_TD:['0'],
+            Rec_Tgt:['3'],
+            Receptions:['2.2'],
+            Rec_Yards:['27.6'],
+            Rec_TD:['0.2'],
+          }],
+          previous:[{
+            Games_Played:['1'],
+            Rush_Att:['0'],
+            Rush_Yards:['0'],
+            Rush_TD:['0'],
+            Rec_Tgt:['2'],
+            Receptions:['1'],
+            Rec_Yards:['8'],
+            Rec_TD:['0'],
+          }],
+          vs_opponent:[{
+            Games_Played:['-'],
+            Rush_Att:['-'],
+            Rush_Yards:['-'],
+            Rush_TD:['-'],
+            Rec_Tgt:['-'],
+            Receptions:['-'],
+            Rec_Yards:['-'],
+            Rec_TD:['-'],
+          }],
+        },
+      ]
+  }
+]
+  },
+  {
+    league: 'NHL',
+    todays_games:[
+      { 
+      id: 'game1',
+      title: 'San Francisco 49ers at Kansas City Chiefs',
+      teamInfo: 'NFL / SF ',
+      date: '2/11 - 6:30 PM EST',
+      players: [
+        {
+          name: 'Brock Purdy - SF',
+          image: Purdy, 
+          teamLogo: Niners,
+          stat:[
+            {
+              book:'PrizePicks',
+              stat: "Passing Yards",
+              Stat_Value: "276.5",
+              Over_Under: "Over"
+            },
+          {
+              book:'PrizePicks',
+              stat: "Rushing Yards",
+              Stat_Value: "27.5",
+              Over_Under: "Under"
+            },
+            {
+              book:'PrizePicks',
+              stat: "Completions",
+              Stat_Value: "276.5",
+              Over_Under: "Over"
+            },
+          {
+              book:'PrizePicks',
+              stat: "Passing TD's",
+              Stat_Value: "1.5",
+              Over_Under: "Under"
+            },
+            {
+              book:'PrizePicks',
+              stat: "Total Touchdowns",
+              Stat_Value: "2.5",
+              Over_Under: "Over"
+            },
+          {
+              book:'PrizePicks',
+              stat: "Turnover",
+              Stat_Value: "0.5",
+              Over_Under: "Under"
+            }
+          ],
+          previousGames: ['300 yards, 3 TDs', '250 yards, 2 TDs'],
+          season:[{
+            Games_Played:['14'],
+            Rush_Att:['0.1'],
+            Rush_Yards:['-0.1'],
+            Rush_TD:['0'],
+            Rec_Tgt:['2.9'],
+            Receptions:['1.8'],
+            Rec_Yards:['23.9'],
+            Rec_TD:['0.1'],
+          }],
+          last_10:[{
+            Games_Played:['10'],
+            Rush_Att:['0.1'],
+            Rush_Yards:['-0.1'],
+            Rush_TD:['0'],
+            Rec_Tgt:['3.3'],
+            Receptions:['2'],
+            Rec_Yards:['23.3'],
+            Rec_TD:['0.1'],
+          }],
+          last_5:[{
+            Games_Played:['5'],
+            Rush_Att:['0.2'],
+            Rush_Yards:['-0.2'],
+            Rush_TD:['0'],
+            Rec_Tgt:['3'],
+            Receptions:['2.2'],
+            Rec_Yards:['27.6'],
+            Rec_TD:['0.2'],
+          }],
+          previous:[{
+            Games_Played:['1'],
+            Rush_Att:['0'],
+            Rush_Yards:['0'],
+            Rush_TD:['0'],
+            Rec_Tgt:['2'],
+            Receptions:['1'],
+            Rec_Yards:['8'],
+            Rec_TD:['0'],
+          }],
+          vs_opponent:[{
+            Games_Played:['-'],
+            Rush_Att:['-'],
+            Rush_Yards:['-'],
+            Rush_TD:['-'],
+            Rec_Tgt:['-'],
+            Receptions:['-'],
+            Rec_Yards:['-'],
+            Rec_TD:['-'],
+          }],
+        },
+      ]
+  }
+]
+  }
+]
+
