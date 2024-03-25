c41183678994c0f63d9a7617af7e2df8055564ad
BVDrewPick <drew@blueverse.club>
2024-03-25 15:44:58 -0500

    Date text affecting rendering on smaller screens fix

--- src/screens/Player-Analysis/playerAnalysis.js
+++ src/screens/Player-Analysis/playerAnalysis.js
@@ -22,42 +22,46 @@ const PlayerAnalysis = () => {
     secondary: '#A9A9A9' 
   };
 
-
   return (
     <div className="space-y-4 mx-auto" style={{ maxWidth: '1700px', width: '100%', padding: '20px', boxSizing: 'border-box' }}>
       <div className="buttons flex justify-start gap-4 mb-6">
-        <button onClick={() => setActiveLeague('NFL')} className="py-2 px-8 border rounded hover:bg-gray-200 transition"  style={{ backgroundColor: '#03FD91' }}>NFL</button>
-        <button onClick={() => setActiveLeague('NHL')} className="py-2 px-8 border rounded hover:bg-gray-200 transition"  style={{ backgroundColor: '#03FD91' }}>NHL</button>
-        <button onClick={() => setActiveLeague('NBA')} className="py-2 px-8 border bg-gray-200 rounded hover:bg-gray-200 transition"  style={{ backgroundColor: '#03FD91' }}>NBA</button>
+        <button onClick={() => setActiveLeague('NFL')} className="py-2 px-8 border rounded hover:bg-gray-200 transition" style={{ backgroundColor: '#03FD91' }}>NFL</button>
+        <button onClick={() => setActiveLeague('NHL')} className="py-2 px-8 border rounded hover:bg-gray-200 transition" style={{ backgroundColor: '#03FD91' }}>NHL</button>
+        <button onClick={() => setActiveLeague('NBA')} className="py-2 px-8 border rounded hover:bg-gray-200 transition" style={{ backgroundColor: '#03FD91' }}>NBA</button>
       </div>
       {filteredGames.map((gameEntry) => (
         gameEntry.todays_games.map((game) => (
-          <div key={game.id} className="p-1 border bg-[#181a24ab] rounded-lg shadow-lg ">
-            <div className="flex items-center gap-2">
-            <div style={{
-                  background: `linear-gradient(${game.awayteamColors?.primary || defaultColors.primary} 2%, ${game.awayteamColors?.secondary || defaultColors.secondary} 30%)`,
-                  padding: '10px', 
-                  borderRadius:'10px',
-                  display: 'inline-block'
-                }}>
-                  <img src={game.away} style={{ maxHeight: '50px', maxWidth: '50px' }} alt="Away Team"/>
+          <div key={game.id} className="p-1 border bg-[#181a24ab] rounded-lg shadow-lg">
+            <div className="flex items-center gap-2 flex-wrap"> {/* Adjusted for wrapping */}
+              <div style={{
+                    background: `linear-gradient(${game.awayteamColors?.primary || defaultColors.primary} 2%, ${game.awayteamColors?.secondary || defaultColors.secondary} 30%)`,
+                    padding: '10px', 
+                    borderRadius: '10px',
+                    display: 'inline-block'
+                  }}>
+                    <img src={game.away} style={{ maxHeight: '50px', maxWidth: '50px' }} alt="Away Team"/>
+              </div>
+              <div className="text-white text-sm px-1 py-.5 rounded-full" style={{
+                    background: '#000000',
+                    margin: '0 -18px',
+                    zIndex: 1,
+                    position: 'relative'
+                  }}>vs</div>
+              <div style={{
+                    background: `linear-gradient(${game.hometeamColors?.primary || defaultColors.primary} 2%, ${game.hometeamColors?.secondary || defaultColors.secondary} 30%)`,
+                    padding: '10px', 
+                    borderRadius: '10px',
+                    display: 'inline-block' 
+                  }}>
+                    <img src={game.home} style={{ maxHeight: '50px', maxWidth: '50px' }} alt="Home Team"/>
+              </div>
+              <div className="flex-1 min-h-[60px] flex items-center gap-2" style={{flexWrap: 'wrap'}}> 
+                <div className="font-bold text-lg cursor-pointer text-white" onClick={() => toggleGame(game.id)}>
+                  {game.title}
                 </div>
-                <div className="text-white text-sm px-1 py-.5 rounded-full" style={{
-                  background: '#000000',
-                  margin: '0 -18px',
-                  zIndex: 1,
-                  position: 'relative'
-                }}>vs</div>
-                <div style={{
-                  background: `linear-gradient(${game.hometeamColors?.primary || defaultColors.primary} 2%, ${game.hometeamColors?.secondary || defaultColors.secondary} 30%)`,
-                  padding: '10px', 
-                  borderRadius:'10px',
-                  display: 'inline-block' 
-                }}>
-                  <img src={game.home} style={{ maxHeight: '50px', maxWidth: '50px' }} alt="Home Team"/>
+                <div className="text-sm text-white font-normal">
+                  {game.date}
                 </div>
-              <div className="font-bold text-lg cursor-pointer text-white" onClick={() => toggleGame(game.id)}>
-                {game.title} <span className="text-sm font-normal">{game.date}</span>
               </div>
             </div>
             {openGameId === game.id && (
