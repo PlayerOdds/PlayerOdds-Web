e7eff2fbf0db48ddbcde17b24ee19ab9f8c06259
BVDrewPick <drew@blueverse.club>
2024-03-25 15:52:12 -0500

    Fixed Player Line to close to Over/Under component on small screens

--- src/screens/DFS-Optimizer/components/PlayerLine.js
+++ src/screens/DFS-Optimizer/components/PlayerLine.js
@@ -2,20 +2,15 @@ import React from 'react';
 
 const PlayerLine = ({ line, betType }) => {
   return (
-      // Adjusting shape and size to match OddsToHit, without changing color schemes
-      <div className=" flex flex-col justify-center items-center space-y-0.5 rounded-xl p-0.25 w-28">
+      <div className=" flex flex-col justify-center items-center space-y-0.5 rounded-xl mr-3 w-28">
           <div className="">
-              {/* Keeping original color and font size */}
               <span className="text-xs text-[#aea6a6] sm:text-xs md:text-xs">LINE</span> 
           </div>
           <div className="">
-              {/* Keeping original color and adjusting size for consistency */}
               <span className="text-base font-bold text-[#f2914b] sm:text-base md:text-base">{line}</span> 
           </div>
           <div className="">
-              {/* Keeping original color and font size */}
               <span className="text-xs font-bold text-white sm:text-xs md:text-xs">{betType}</span>
-
           </div>
       </div>
   );
