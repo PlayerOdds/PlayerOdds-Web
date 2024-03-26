32e550211abb9f03f7406ca2cc11008f45ea8112
BVDrewPick <drew@blueverse.club>
2024-03-26 12:24:26 -0500

    Updated Player Details Styles

--- src/screens/Player-Analysis/components/PlayerDetails.js
+++ src/screens/Player-Analysis/components/PlayerDetails.js
@@ -51,8 +51,9 @@ const PlayerDetails = ({ player, sport }) => {
           {player.stat.map((statItem, idx) => {
             const overStats = getOverStatsFromStatItem(statItem);
             return (
-              <div key={idx} className="border-pink-500 rounded-xl px-16 py-8 flex flex-col items-center justify-center text-center" style={{ marginRight: '-2px', marginLeft: '-2px' }}>
-                <div className="bg-[#1f232a] text-white rounded-xl flex p-4 justify-around items-center w-full mb-2 border-rounded">
+            <div key={idx} className="border-r-4 border-r-[#1f232a] px-14 py-4 flex flex-col items-center justify-center text-center" 
+                style={{ marginRight: '-2px', marginLeft: '-2px' }}>
+                <div className="text-white rounded-xl flex p-4 justify-around items-center w-full mb-2 border-rounded">
                   {overStats.map((stat, index) => (
                     <div key={index} className="text-sm flex flex-col items-center p-2">
                       <div>{`${stat.key}:`}</div>
@@ -60,7 +61,7 @@ const PlayerDetails = ({ player, sport }) => {
                     </div>
                   ))}
                 </div>
-                <div className="bg-slate-500 w-36 h-24 rounded flex flex-col items-center justify-center text-sm">
+                <div className="bg-[#1f232a] w-36 h-24 rounded flex flex-col items-center justify-center text-sm">
                   <div className='text-white mb-2'>{statItem.stat}</div>
                   <div className='text-yellow-400 text-xl'>{`${statItem.Stat_Value}`}</div>
                 </div>
