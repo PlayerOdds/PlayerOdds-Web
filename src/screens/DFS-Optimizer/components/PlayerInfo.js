14a7fb395068de873727b319aa103b547ed0792b
BVDrewPick <drew@blueverse.club>
2024-03-26 14:05:22 -0500

    Game Time Changes

--- src/screens/DFS-Optimizer/components/PlayerInfo.js
+++ src/screens/DFS-Optimizer/components/PlayerInfo.js
@@ -1,13 +1,15 @@
 import React from "react"
 
-const PlayerInfo = ({name, teamInfo, matchup, playerImage }) => {
+const PlayerInfo = ({name, teamInfo, matchup, playerImage, matchupDate }) => {
   return (
   <div className="flex items-center space-x-6">
     <img src={playerImage} alt={playerImage} className="w-16 h-16" />
       <div>
         <p className='text-l font-bold mb-1'>{name}</p>
         <p className="text-sm mb-1 sm:text-xs ">{teamInfo}</p>
-        <p className="text-xs">{matchup}</p>
+        <p className="text-xs">{matchup} -</p>
+        <p className="text-xs">{matchupDate}</p>
+
       </div>
   </div>
   )
