fed02cf27cb2a6c9a6057d73a86dda6d7fc4ae5b
BVDrewPick <drew@blueverse.club>
2024-03-29 14:26:41 -0500

    Updated FAQ

--- src/screens/HomeScreen/components/FAQ.js
+++ src/screens/HomeScreen/components/FAQ.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import FAQItem from './FAQItem'; // Make sure this path matches the location of your FAQItem component
+import FAQItem from './FAQItem'; 
 
 const FAQ = () => {
   const faqs = [
@@ -11,13 +11,25 @@ const FAQ = () => {
       question: "What is +EV Betting? ",
       answer: "Postive Expected Value or +EV Betting is where according to the market that bet is undervalued and has more potential to make profit!"
     },
+    {
+      question: "How Will I Access The Server? ",
+      answer: `Once you place your purchase you will automatically receive the "VIP MEMBER" role in discord and all the premium channels will unlock. You will be given an invite link to join. If you cancel your membership these channels will disappear.`
+    },
+    {
+      question: "Why can't I see the premium chat? ",
+      answer: "Make sure you have your discord connected to Whop and you properly claim your roles after purchasing. You can find a confirmation email with the option to claim your role. "
+    },
+    {
+      question: "What type of sports betting does the group focus on? ",
+      answer: "We will primarily focus around Daily Fantasy Sports for the time being. "
+    },
     // Add more FAQs here
   ];
 
   return (
     <div className="max-w-4xl mx-auto my-10 p-5 bg-[#141c22] rounded-lg shadow">
       <h1 className="text-5xl font-bold mb-10 text-[#17D475] text-center">FAQ</h1>
-      <div className="divide-y  divide-gray-200">
+      <div className="divide divide-gray-200">
         {faqs.map((faq, index) => (
           <FAQItem key={index} question={faq.question} answer={faq.answer} />
         ))}
--- src/screens/HomeScreen/components/FAQItem.js
+++ src/screens/HomeScreen/components/FAQItem.js
@@ -4,17 +4,17 @@ const FAQItem = ({ question, answer }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="mb-4">
-      <div className="bg-white shadow-md rounded-lg overflow-hidden">
+    <div className="mb-4 bg-[#141c22]" >
+      <div className="shadow-md bg-[#141c22] rounded-lg overflow-hidden">
         <button
-          className="flex justify-between items-center w-full text-left text-lg font-semibold py-2 px-4 bg-[gray-100] hover:bg-gray-200"
+          className="flex justify-between items-center w-full text-left text-lg font-semibold py-2 px-4 bg-[gray-100] text-white"
           onClick={() => setIsOpen(!isOpen)}
         >
           {question}
           <span className="text-xl">{isOpen ? '−' : '+'}</span>
         </button>
         {isOpen && (
-          <div className="px-4 py-2 text-gray-700 bg-white">
+          <div className="px-4 py-2 text-white bg-[#141c22]">
             {answer}
           </div>
         )}
